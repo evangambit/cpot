5f1048897e19911f789244c3474d244be21024c9
Morgan Redding <evangambit@gmail.com>
2023-09-30 12:59:43 -0700

    Migrate build_test_index.cpp to gtest

--- tests/build_test_index.cpp
+++ tests/build_test_index.cpp
@@ -1,5 +1,4 @@
-// clang++ build_test_index.cpp -std=c++2a -O3 -o build_test
-// rm -rf test-index*; ./build_test
+#include "gtest/gtest.h"
 
 #include <cstdint>
 #include <deque>
@@ -8,21 +7,15 @@
 #include <fstream>
 #include <vector>
 
-#include "src/common/InvertedIndex.h"
-#include "src/common/utils.h"
-#include "src/uint64/UInt64Row.h"
+#include "../src/common/InvertedIndex.h"
+#include "../src/common/utils.h"
+#include "../src/uint64/UInt64Row.h"
 
 using namespace cpot;
 
-template<class T>
-void assert_eq(const T& a, const T& b) {
-  if (!(a == b)) {
-    std::cout << a << " != " << b << std::endl;
-    assert(false);
-  }
-}
+namespace {
 
-int main() {
+TEST(BuildIndexTests, Fifty) {
   const uint64_t N = 500'000;
   {
     auto t0 = std::chrono::high_resolution_clock::now();
@@ -35,7 +28,7 @@ int main() {
       }
     }
     auto t1 = std::chrono::high_resolution_clock::now();
-    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count() << std::endl;
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count() << "ms" << std::endl;
   }
 
   {
@@ -45,11 +38,20 @@ int main() {
     std::cout << index.all(1).size() << std::endl;
     assert(index.all(1).size() == N);
     auto t1 = std::chrono::high_resolution_clock::now();
-    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count() << std::endl;
-    assert_eq(index.all(2).size(), size_t(250'000));
-    assert_eq(index.all(3).size(), size_t(166'666));
-    assert_eq(index.all(4).size(), size_t(125'000));
-    assert_eq(index.all(5).size(), size_t(100'000));
-    assert_eq(index.all(6).size(), size_t( 83'333));
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count() << "ms" << std::endl;
+    ASSERT_EQ(index.all(2).size(), size_t(250'000));
+    ASSERT_EQ(index.all(3).size(), size_t(166'666));
+    ASSERT_EQ(index.all(4).size(), size_t(125'000));
+    ASSERT_EQ(index.all(5).size(), size_t(100'000));
+    ASSERT_EQ(index.all(6).size(), size_t( 83'333));
   }
 }
+
+}  // namespace
+
+
+int main() {
+  testing::InitGoogleTest();
+  return RUN_ALL_TESTS();
+}
+
