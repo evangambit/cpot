91984904065f454bcca1c6415471d4ece877504e
Morgan Redding <evangambit@gmail.com>
2023-09-30 13:00:47 -0700

    Add script to run all tests

---
+++ tests/run.sh
@@ -0,0 +1,13 @@
+
+
+for fn in $(ls tests)
+do
+  if [ "$fn" = "run.sh" ]; then
+    continue
+  fi
+  rm test-index*
+  rm a.out
+  echo $fn
+  clang++ tests/${fn} -I/opt/homebrew/Cellar/googletest/1.14.0/include -std=c++20 -L/opt/homebrew/Cellar/googletest/1.14.0/lib -lgtest && ./a.out
+done
+
