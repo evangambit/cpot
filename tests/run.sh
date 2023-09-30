cf3190d93a3be7b8802e92c3227de4c2f12ba18e
Morgan Redding <evangambit@gmail.com>
2023-09-30 13:21:03 -0700

    Add ability to run a single test file

--- tests/run.sh
+++ tests/run.sh
@@ -1,13 +1,18 @@
 
-
-for fn in $(ls tests)
-do
-  if [ "$fn" = "run.sh" ]; then
-    continue
-  fi
+if ["$1" == ""]; then
+  for fn in $(ls tests)
+  do
+    if [ "$fn" = "run.sh" ]; then
+      continue
+    fi
+    rm test-index*
+    rm a.out
+    echo $fn
+    clang++ tests/${fn} -I/opt/homebrew/Cellar/googletest/1.14.0/include -std=c++20 -L/opt/homebrew/Cellar/googletest/1.14.0/lib -lgtest && ./a.out
+  done
+else
   rm test-index*
   rm a.out
-  echo $fn
-  clang++ tests/${fn} -I/opt/homebrew/Cellar/googletest/1.14.0/include -std=c++20 -L/opt/homebrew/Cellar/googletest/1.14.0/lib -lgtest && ./a.out
-done
+  clang++ $1 -I/opt/homebrew/Cellar/googletest/1.14.0/include -std=c++20 -L/opt/homebrew/Cellar/googletest/1.14.0/lib -lgtest && ./a.out
+fi
 
